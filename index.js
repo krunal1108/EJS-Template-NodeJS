fcea983e2079e0d58a0df256e7bcc8e1549bb978
Krunal Kotadiya <147746910+krunal1108@users.noreply.github.com>
2024-08-21 11:12:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,56 +15,10 @@ app.set('views',mypath);
 // Views Directory
 app.set('views', path.join(__dirname, '/views'));
 
-
-
-
-// Set up Static Files (css, images, etc.)
-// app.use(express.static(path.join(__dirname, 'views')));
-
-
 app.use(express.static(mypath));
 
 // Use the routes
 app.use('/', router);
-
-
-
-// app.get('/', (req, res)=>{
-//     console.log("Home Route");
-//     res.render('index');
-// });
-
-// app.get('/about', (req, res)=>{
-//     console.log("About Route");
-//     res.render('about');
-// });
-
-// app.get('/gallery', (req, res)=>{
-//     console.log("Gallery Route");
-//     res.render('gallery');
-// });
-
-
-// app.get('/e-shopping', (req, res)=>{
-//     console.log("e-shopping Route");
-//     res.render('e-shopping');
-// });
-
-
-// app.get('/contact', (req, res)=>{
-//     console.log("contact Route");
-//     res.render('contact');
-// });
-
-
-// 404 Error Handling Middleware
-// app.use((req, res, next) => {
-//     res.status(404).render('404', { title: '404 - Page Not Found' });
-// });
-
-
-
-
 
 app.listen(PORT, (error)=>{
     if(!error){
